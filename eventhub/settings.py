ebe3fc6dd2097a33d3b78b9e827ae77555767fe1
cari395 <carinimatu11@live.com.ar>
2025-06-12 16:52:00 -0300

    Se agrega el script para la entrega continua al hacer un release

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
@@ -104,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATIC_URL = "/static/"
 
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split()
 # Bootstrap 5
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
